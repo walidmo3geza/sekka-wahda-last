f89d14ec0493f5051e5ecd47865da2c4acb749cd
Your Name <you@example.com>
2020-08-12 11:36:33 +0200

    second commit

--- resources/views/show_trip_details.blade.php
+++ resources/views/show_trip_details.blade.php
@@ -9,16 +9,13 @@
         <h3>DateOfTrip - {{ $data->DateOfTrip }}</h3>
         <h3>TimeOfTrip - {{ $data->TimeOfTrip }} </h3>
         <ul><h3>Driver Details</h3>
-            <?php
-            $driver = \App\UserMaster::find($data->DriverId);
-            ?>
-            <li><h5>ID - {{ $driver->UserID }}</h5></li>
-            <li><h5>UserName - {{ $driver->UserName }}</h5> </li>
-            <li><h5>Email - {{ $driver->UserEmailID }}</h5></li>
-            <li><h5>SSN - {{ $driver->SSN }} </h5></li>
-            <li><h5>Name - {{ $driver->FullName }}</h5></li>
-            <li><h5>Phone Number - {{ $driver->PhoneNumber }}</h5> </li>
-            <li><h5>Total Rate - {{ $driver->DriverTotalRate }}</h5> </li>
+            <li><h5>ID - {{ $data->driver->UserID }}</h5></li>
+            <li><h5>UserName - {{ $data->driver->UserName }}</h5> </li>
+            <li><h5>Email - {{ $data->driver->UserEmailID }}</h5></li>
+            <li><h5>SSN - {{ $data->driver->SSN }} </h5></li>
+            <li><h5>Name - {{ $data->driver->FullName }}</h5></li>
+            <li><h5>Phone Number - {{ $data->driver->PhoneNumber }}</h5> </li>
+            <li><h5>Total Rate - {{ $data->driver->DriverTotalRate }}</h5> </li>
         </ul>
     </div>
 @endsection
